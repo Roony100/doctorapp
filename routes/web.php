f1ca283bb1390b2e579676bbd790453f77fbfe1b
roony <roony5ml@gmail.com>
2026-07-30 05:54:54 -0700

    Add all models with relationships, add role and active to users table

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->middleware('checkrole:admin')->name('admin.dashboard');
+
+    Route::get('/doctor/dashboard', function () {
+        return view('doctor.dashboard');
+    })->middleware('checkrole:doctor')->name('doctor.dashboard');
+
+    Route::get('/patient/dashboard', function () {
+        return view('patient.dashboard');
+    })->middleware('checkrole:patient')->name('patient.dashboard');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
